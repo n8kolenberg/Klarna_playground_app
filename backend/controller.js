8f2d689ff14f6fd80a51aee73d70c8e196c1809e
Nathaniel Kolenberg <nkolenberg@gmail.com>
2019-07-29 13:40:26 +0200

    prepare backend for placing order with authToken and userData passed by the frontend

--- backend/controller.js
+++ backend/controller.js
@@ -27,7 +27,7 @@ module.exports = {
         //Execute the request
         axios(options)
         .then((response)=> {
-            console.log(`Request sent successfully through Backend. Session id: ${response.data.session_id}`);
+            console.log(`Create Session Request sent successfully through Backend. Session id: ${response.data.session_id}`);
             callback(response);
         })
         .catch((error) => {
@@ -40,7 +40,34 @@ module.exports = {
         
     },
 
-    placeOrder() {
+    placeOrder(payload, callback) {
+         //digest
+         let digest = createDigest();
+        
+         //options to send along in the request
+         let options = {
+             method: 'POST',
+             url: `${config.endpoint}/payments/v1/authorizations/${payload.authToken}/order`,
+             headers: {
+                 'Content-Type': 'application/json',
+                 'Authorization': `Basic ${digest}`
+             },
+             data: payload.userData,
+         };
+ 
+         //Execute the request
+         axios(options)
+         .then((response)=> {
 
+             console.log(`Place Order Request sent successfully through Backend. Order id: ${response.data.order_id}`);
+             callback(response.data);
+         })
+         .catch((error) => {
+             //Modifying the data so error response can be used by callback in the same way as success message
+             let response = {}
+             response.data = {"message": error.error_messages, "correlation_id": error.correlation_id, "error_code": error.error_code};
+             console.log(`Error with placeOrder request in Backend: ${error}`)
+             callback(response);
+         });
     }
 };
--- backend/routes/place-order.js
+++ backend/routes/place-order.js
@@ -1,4 +1,19 @@
 const express = require('express');
 const router = express.Router({mergeParams: true});
+const controller = require('../controller')
+
+router.use((req, res, next) => {
+    console.log("routing in progress");
+    next();
+});
+
+router.post('/', (req, res)=>{
+//payload from the frontend should now include the authorization_token
+    let payload = req.body;
+    controller.placeOrder(payload, (response)=> {
+        //Sending the response to the frontend
+        res.send(response);
+    });
+});
 
 module.exports = router;
